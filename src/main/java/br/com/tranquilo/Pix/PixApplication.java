88a014e17009b0ffa2bb26371711524528d1976c
Gigliárly Marcelino <60317202+GigliarlyM@users.noreply.github.com>
2024-04-24 09:59:27 -0300

    Inicio

---
+++ src/main/java/br/com/tranquilo/Pix/PixApplication.java
@@ -0,0 +1,13 @@
+package br.com.tranquilo.Pix;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PixApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PixApplication.class, args);
+	}
+
+}

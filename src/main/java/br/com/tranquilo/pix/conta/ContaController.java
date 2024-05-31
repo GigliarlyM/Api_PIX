bd00de1ac584f7645cb3c820c52cca8fbdc8d0ce
Gigliárly Marcelino <gigliarlymarcelino@gmail.com>
2024-05-31 19:47:15 -0300

    pequenas_alteracoes

--- src/main/java/br/com/tranquilo/pix/conta/ContaController.java
+++ src/main/java/br/com/tranquilo/pix/conta/ContaController.java
@@ -3,8 +3,7 @@ package br.com.tranquilo.pix.conta;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -15,9 +14,17 @@ public class ContaController {
     @Autowired
     private ContaService service;
 
+    @GetMapping("")
     public ResponseEntity<List<Conta>> listarContas() {
         List<Conta> contas = service.listarContas();
 
         return new ResponseEntity<List<Conta>>(contas, HttpStatus.OK);
+    }
+
+    @PostMapping
+    public ResponseEntity<Conta> adicionarConta(@RequestBody Conta conta) {
+        Conta contaCriada = service.adicionarNovaConta(conta);
+
+        return new ResponseEntity<Conta>(contaCriada, HttpStatus.OK);
     }
 }
